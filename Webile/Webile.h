47abf09e0637190372e45a4e0721ed4d2f72304f
Siva Nasina <siva@webileapps.com>
2019-07-30 19:24:50 +0530

    Initial project setup

---
+++ Webile/Webile.h
@@ -0,0 +1,19 @@
+//
+//  Webile.h
+//  Webile
+//
+//  Created by Siva on 30/07/19.
+//  Copyright © 2019 Siva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Webile.
+FOUNDATION_EXPORT double WebileVersionNumber;
+
+//! Project version string for Webile.
+FOUNDATION_EXPORT const unsigned char WebileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Webile/PublicHeader.h>
+
+
